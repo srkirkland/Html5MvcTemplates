83ee1cc8567b1bd5c5907738a6113fb5f03657bf
Scott Kirkland <srkirkland@gmail.com>
2011-03-29 20:44:24 -0700

    added some basic navigation an a sample model

---
+++ src/html5Templates.Web/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using html5Templates.Web.Models;
+
+namespace html5Templates.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KitchenSink()
+        {
+            var kitchenSink = new KitchenSink();
+
+            return View(kitchenSink);
+        }
+    }
+}
---
+++ src/html5Templates.Web/Models/KitchenSink.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace html5Templates.Web.Models
+{
+    public class KitchenSink
+    {
+        public string RegularString { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+    }
+}
